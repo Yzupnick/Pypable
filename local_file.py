76d8467ca08e45a05f0f1bc53811681b20aa6d12
Chanan Zupnick <chazup@gmail.com>
2015-10-18 16:36:20 -0400

    added class for azure blob storage

--- local_file.py
+++ local_file.py
@@ -1,4 +1,5 @@
 import os
+import azure
 
 class BaseDestination(object):
     def __init__(self,parser):
@@ -37,3 +38,36 @@ class LocalFile(BaseDestination):
     def _save_string_as_file(self,args, contants):
         with open(args.name, 'w') as new_file:
             new_file.write(contants)
+
+
+class AzureBlobStorage(object):
+    name = "blobs"
+    help_text = "save to azure blob storage"
+
+    def __init__(self,parser):
+        parser.add_argument('--acount', dest='acount')
+        parser.add_argument('--key', dest='key')
+        parser.add_argument('--container', dest='container')
+        super().__init__(parser)
+
+    def __call__(self,args, contents):
+        self.blob_service = azure.storage.BlobService(account_name=args.account, account_key=args.key)
+        if args.files:
+            self._copy_files(args, contents)
+        else:
+            self._send_string(args, contents)
+
+    def _copy_files(self,args, file_names):
+        for file_name in [fn.strip() for fn in file_names.split('\n')]:
+            self.blob_service.put_block_blob_from_path(
+                args.container,
+                file_name,
+                file_name
+            )
+
+    def _send_string(self,args, contants):
+        self.blob_service.put_block_blob_from_text(
+            args.container,
+            args.name,
+            contents
+        )
